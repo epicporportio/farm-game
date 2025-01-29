76dae2b128744f4789a2f3b4d996e577d727ff22
epicporportio <136861968+epicporportio@users.noreply.github.com>
2025-01-29 02:03:32 -0700

    Web export

--- scripts/universal_crop_button.gd
+++
@@ -1,9 +0,0 @@
-extends VBoxContainer
-
-signal pressed_id(plant_id)
-@onready var universal_button: TextureButton = $UniversalButton
-@onready var universal_label: Label = $UniversalLabel
-@onready var plant_id = 0
-
-func _on_universal_button_pressed() -> void:
-	pressed_id.emit(plant_id)
